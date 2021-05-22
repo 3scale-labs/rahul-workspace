5d1cdb6314a3fa1921db65018082bd17d0e7f499
Rahul Anand <rahulanand16nov@gmail.com>
2021-05-22 14:14:17 +0530

    Changed consumption of Q through on_queue_ready()

--- message-queue-poc/singleton-service/src/lib.rs
+++ message-queue-poc/singleton-service/src/lib.rs
@@ -1,7 +1,7 @@
 use log::debug;
 use proxy_wasm::traits::{Context, RootContext};
 use proxy_wasm::types::LogLevel;
-use std::{time::Duration, str::from_utf8};
+use std::{str::from_utf8};
 use proxy_wasm::hostcalls::{
     register_shared_queue,
     dequeue_shared_queue
@@ -9,7 +9,6 @@ use proxy_wasm::hostcalls::{
 
 
 const QUEUE_NAME: &str = "message_queue"; // Note: Name of the queue can be set in the static config as well
-const TICK_DURATION: Duration = Duration::from_secs(2); // on_tick() will be called every duration unit described here
 
 #[no_mangle]
 pub fn _start() {
@@ -32,12 +31,11 @@ impl RootContext for SingletonService {
         // Register MessageQ to receive info from cache filters
         if let Ok(q_id) = register_shared_queue(QUEUE_NAME) { self.queue_id = Some(q_id); }
         debug!("Registered new message queue with id: {} with context id: {}", self.queue_id.unwrap(),self.root_context_id);
-        self.set_tick_period(TICK_DURATION);
         true
     }
 
-    // Every tick duration, message is consumed from shared queue.
-    fn on_tick(&mut self) {
+    // As soon as message queue is ready, message is consumed from it.
+    fn on_queue_ready(&mut self, _queue_id: u32) {
         
         match dequeue_shared_queue(self.queue_id.unwrap()) {
             Ok(message_res) => {
