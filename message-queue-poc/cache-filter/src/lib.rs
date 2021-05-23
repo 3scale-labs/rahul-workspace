8de5dc8a7b41a0871850f0ae73f34a58242b197e
Rahul Anand <rahulanand16nov@gmail.com>
2021-05-23 17:59:54 +0530

    Finished testing enqueue API with full consumption rate through on_queue_ready

--- message-queue-poc/cache-filter/src/lib.rs
+++ message-queue-poc/cache-filter/src/lib.rs
@@ -1,7 +1,8 @@
 use log::debug;
+use std::time::SystemTime;
 use proxy_wasm::{
     traits::*,
-    hostcalls::{resolve_shared_queue, enqueue_shared_queue},
+    hostcalls::{resolve_shared_queue, enqueue_shared_queue, get_current_time},
     types::{Action, LogLevel},
 };
 
@@ -29,14 +30,30 @@ impl HttpContext for CacheFilter {
         // Get the queue_id to pass info to the singleton service
         let queue_id = resolve_shared_queue(VM_ID, QUEUE_NAME).unwrap();
 
+        // Benchmarking setup
+        let mut total_time = 0.0;
+        let mut headers_count = 0;
+
         // Enquing request headers in Message Queue (dummy data) for testing
         for (name, value) in &self.get_http_request_headers() {
+            headers_count = headers_count + 1;
             debug!("In WASM : #{} -> Adding {}: {} to the message queue", self.context_id, name, value);
+
+            //We cannot use Instant::now() because it causes panic on every WASM platform.
+            let start_time = get_current_time().unwrap().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs_f64();
+
             match enqueue_shared_queue(queue_id.unwrap(), Some(value.as_bytes())) {
                 Ok(_t) => debug!("Enqued: '{}' on Queue with id: {} successfully",value,queue_id.unwrap()),
                 Err(e) => debug!("Enquing queue with id: {} failed due to: {:?}",queue_id.unwrap(),e)
             }
+
+            let end_time = get_current_time().unwrap().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs_f64();
+            total_time = end_time - start_time;
         }
+        
+        let average_time = (total_time/headers_count as f64).to_string();
+        debug!("Average time to enqueue headers: {} ", average_time);
+        if let Ok(_) = enqueue_shared_queue(queue_id.unwrap(), Some(average_time.as_bytes())) {}
     Action::Continue
     }
 }
--- message-queue-poc/singleton-service/src/lib.rs
+++ message-queue-poc/singleton-service/src/lib.rs
@@ -1,7 +1,7 @@
 use log::debug;
 use proxy_wasm::traits::{Context, RootContext};
+use std::time::Duration;
 use proxy_wasm::types::LogLevel;
-use std::{str::from_utf8};
 use proxy_wasm::hostcalls::{
     register_shared_queue,
     dequeue_shared_queue
@@ -9,18 +9,20 @@ use proxy_wasm::hostcalls::{
 
 
 const QUEUE_NAME: &str = "message_queue"; // Note: Name of the queue can be set in the static config as well
-
+const PUBLISH_TIME: u64 = 30; // This time should be sufficient so that all requests are processed
 #[no_mangle]
 pub fn _start() {
     proxy_wasm::set_log_level(LogLevel::Debug);
     proxy_wasm::set_root_context(|root_context_id| -> Box<dyn RootContext> {
-        Box::new(SingletonService {root_context_id, queue_id: None})
+        Box::new(SingletonService {root_context_id, queue_id: None, enqueue_time: 0.0,  enqueue_count: 0})
     });
 }
 
 struct SingletonService {
     root_context_id: u32,
-    queue_id: Option<u32>
+    queue_id: Option<u32>,
+    enqueue_time: f64,
+    enqueue_count: u32,
 }
 
 impl Context for SingletonService {}
@@ -31,6 +33,7 @@ impl RootContext for SingletonService {
         // Register MessageQ to receive info from cache filters
         if let Ok(q_id) = register_shared_queue(QUEUE_NAME) { self.queue_id = Some(q_id); }
         debug!("Registered new message queue with id: {} with context id: {}", self.queue_id.unwrap(),self.root_context_id);
+        self.set_tick_period(Duration::from_secs(PUBLISH_TIME));
         true
     }
 
@@ -40,10 +43,22 @@ impl RootContext for SingletonService {
         match dequeue_shared_queue(self.queue_id.unwrap()) {
             Ok(message_res) => {
                 if let Some(message_bytes) = message_res {
-                    debug!("RCID#{}: Consumed following message from the shared queue: {}", self.root_context_id,from_utf8(&message_bytes).unwrap());
+                    let message = String::from_utf8(message_bytes).unwrap();
+                    if message.contains("0.00") {
+                        self.enqueue_time = self.enqueue_time + message.parse::<f64>().unwrap();
+                        self.enqueue_count = self.enqueue_count + 1;
+                    }
+                    debug!("RCID#{}: Consumed following message from the shared queue: {}", self.root_context_id,message);
                 }
             },
             Err(e) => debug!("Consuming message queue failed due to: {:?}",e)
         }
+    }
+
+    // After every tick duration, stats are published on terminal and set to initial values
+    fn on_tick(&mut self) {
+        debug!("Reporting enquing stats: Total time: {} Total Count: {}", self.enqueue_time, self.enqueue_count);
+        self.enqueue_time = 0.0;
+        self.enqueue_count = 0;
     }
 }
